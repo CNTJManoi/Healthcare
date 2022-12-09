4dd2992fe9893f6d7b6e3105a381b54c6feef5c5
Алексей Коровченко <lecha3525@gmail.com>
2022-12-10 00:24:18 +0700

    Add support URL link in image wpf

--- Healthcare.WPF/ViewModels/AppointmentDoctorViewModel.cs
+++ Healthcare.WPF/ViewModels/AppointmentDoctorViewModel.cs
@@ -17,8 +17,8 @@ namespace Healthcare.WPF.ViewModels
             ListBoxItems = new ObservableCollection<DoctorViewModel>();
             ListBoxItems.Add(new DoctorViewModel("C:\\Users\\lecha\\source\\repos\\Healthcare\\Healthcare.WPF\\Views\\Pages\\Main\\Y1g2xS08uZE.jpg"
             ,"Терапевт", "Жмышенко Валерий Иванович", "09:00 - 12:00"));
-            ListBoxItems.Add(new DoctorViewModel("C:\\Users\\lecha\\source\\repos\\Healthcare\\Healthcare.WPF\\Views\\Pages\\Main\\Y1g2xS08uZE.jpg"
-                , "Терапевт", "Жмышенко Валерий Иванович", "09:00 - 12:00"));
+            ListBoxItems.Add(new DoctorViewModel("https://www.meme-arsenal.com/memes/17546868fd82d11133e23d38fe7e8d5e.jpg"
+                , "Уролог", "Владислав Ярослав Владленович", "10:00 - 15:00"));
             ListBoxItems.Add(new DoctorViewModel("C:\\Users\\lecha\\source\\repos\\Healthcare\\Healthcare.WPF\\Views\\Pages\\Main\\Y1g2xS08uZE.jpg"
                 , "Терапевт", "Жмышенко Валерий Иванович", "09:00 - 12:00"));
             ListBoxItems.Add(new DoctorViewModel("C:\\Users\\lecha\\source\\repos\\Healthcare\\Healthcare.WPF\\Views\\Pages\\Main\\Y1g2xS08uZE.jpg"
--- Healthcare.WPF/ViewModels/Models/DoctorViewModel.cs
+++ Healthcare.WPF/ViewModels/Models/DoctorViewModel.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Healthcare.WPF.ViewModels.Models
 {
     public class DoctorViewModel : ViewModelBase
     {
-        public string Path { get; set ; }
+        static ImageSourceConverter imageSourceConverter = new ImageSourceConverter();
+        public ImageSource ImageOnScreen { get; set ; }
         public string Name { get; set; }
         public string Time { get; set; }
         public string Specialization { get; set; }
         public DoctorViewModel(string path, string specialization, string fio, string time)
-        {
-            Path = path;
+        { 
+            ImageOnScreen = (ImageSource)imageSourceConverter.ConvertFrom(path);
             Name = fio;
             Time = time;
             Specialization = specialization;
-            OnPropertyChanged(nameof(Path));
+            OnPropertyChanged(nameof(ImageOnScreen));
             OnPropertyChanged(nameof(Name));
             OnPropertyChanged(nameof(Time));
             OnPropertyChanged(nameof(Specialization));
